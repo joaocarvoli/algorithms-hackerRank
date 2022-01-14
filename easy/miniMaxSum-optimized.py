7a66f7320ac1976cefff9b6be18b79df1bb07d73
Joao Victor <joaocarvoli@hotmail.com>
2022-01-13 22:40:57 -0300

    miniMaxSum is solved but is missing code complexity

--- easy/miniMaxSum-optimized.py
+++ easy/miniMaxSum-optimized.py
@@ -1,11 +1,22 @@
+# Functions complexity: min: O(n), max: O(n), list.index: O(n), len: O(1), pop: O(n), insert: O(n)
 def miniMaxSum(arr):
-    minimum = set([min(arr)])
-    maximum = set([max(arr)])
-    return print(sum(set(arr) ^ maximum), sum(set(arr) ^ minimum))
+    mininum = min(arr)
+    maximum = max(arr)
+    
+    arr.pop(arr.index(maximum)) # Popping the max in the array
+    minimumSum = sum(arr)
+    arr.pop(arr.index(mininum)) # Popping the max in the array
+    
+    arr.insert(1, maximum)      # Inserting the maximum in the array to calculate the another sum
+    maximumSum = sum(arr)
 
+    
+    return print(minimumSum, maximumSum)
+
+# Test cases
 
 miniMaxSum([1,2,3,4,5])
-# Strategy: I take the minimum and maximum of the arr and for each one, we calculate its symmetric difference.
-# Example: arr = [1,2,3,4,5], mininum = 1 and maximum = 5.
-# The symmetric difference in relation to arr using maximum is [1,2,3,4]
-# The symmetric difference in relation to arr using minimum is [2,3,4,5]    
+miniMaxSum([5,5,5,5,5])
+miniMaxSum([1,1,2,2,3,4,5])
+
+# Code complexity: 
--- easy/miniMaxSum.py
+++ easy/miniMaxSum.py
@@ -1,5 +1,5 @@
 # Time complexity of some functions: https://wiki.python.org/moin/TimeComplexity
-# Functions complexity: [min and max: O(n)], [len: O(1)]
+# Functions complexity: [min and max: O(n)], [len: O(1)], [list.index: O(n)]
 
 def miniMaxSum(arr):
     minimum = min(arr)
@@ -10,8 +10,27 @@ def miniMaxSum(arr):
     for i in range(len(arr)):
         if arr[i] != maximum:
             minimumSum = minimumSum + arr[i]
+        elif arr[i] == maximum:
+            if flagTomax > 0:
+                minimumSum = minimumSum + arr[i]
+            flagTomax = flagTomax + 1
+    
+
+    for i in range(len(arr)):
         if arr[i] != minimum:
             maximumSum = maximumSum + arr[i]
+        elif arr[i] == minimum:
+            if flagTomin > 0:
+                maximumSum = maximumSum + arr[i]
+            flagTomin = flagTomin + 1
+
+
     return print(minimumSum, maximumSum)
 
+# Test cases
+
+miniMaxSum([1,2,3,4,5])
 miniMaxSum([5,5,5,5,5])
+miniMaxSum([1,1,2,2,3,4,5])
+
+# Code complexity:
