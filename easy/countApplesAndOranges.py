f80f36f03af80f64e317c4e8e970f07afe792ea5
Joao Victor <joaocarvoli@hotmail.com>
2022-04-19 20:15:00 -0300

    Today problem: Count of Apples and Oranges

---
+++ easy/countApplesAndOranges.py
@@ -0,0 +1,18 @@
+# Problem reference: https://www.hackerrank.com/challenges/apple-and-orange/problem?isFullScreen=false
+
+def countApplesAndOranges(houseBegin,houseEnd,appleTree,orangeTree,apples,oranges):
+    countApples, countOranges = [0,0]
+
+    for apple in apples:
+        if houseBegin <= (apple + appleTree) and (apple + appleTree) <= houseEnd:
+            countApples = countApples + 1
+    for orange in oranges:
+        if houseBegin <= (orange + orangeTree) and (orange + orangeTree) <= houseEnd:
+            countOranges = countOranges + 1
+
+    print(countApples)
+    print(countOranges)
+    return None
+
+
+print(countApplesAndOranges(7,10,4,12,[2,3,-4],[3,-2,-4]))
