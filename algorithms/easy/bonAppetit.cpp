9fce310b19b31d82b566dedc4128bf63f536d5ff
João Victor Carvalho <joaocarvoli@pop-os.localdomain>
2022-12-13 23:05:00 -0300

    solving today problem

---
+++ algorithms/easy/bonAppetit.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+void bonAppetit(vector<int> bill, int declinedItemIndex, int valueCharged){
+  int similarCost = 0;
+  for (int i = 0; i < bill.size() ; i++) if (i != declinedItemIndex) similarCost += bill[i];
+  if (valueCharged <= similarCost / 2) printf("Bon Appetit"); 
+  else printf("%d", abs((similarCost / 2) - valueCharged));
+} 
+
+
+int main(){
+  vector<int> bill = {72,53,60,66,90,62,12,31,36,94};
+  bonAppetit(bill, 6, 288); // 6
+}
